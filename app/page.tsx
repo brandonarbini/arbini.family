39ca40f04ca21d4d73cbaeb225cd75daa1a9d530
Brandon Arbini <b@arbini.dev>
2026-01-11 22:07:26 -0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white dark:bg-black">
+      <h1 className="text-2xl font-medium text-black dark:text-white">arbini.family</h1>
+    </div>
+  );
+}
